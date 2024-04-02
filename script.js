4279502ae67680d32cf6f269561b3a46c8d3d75b
Muffin <loan.roze@efrei.net>
2024-04-02 10:11:12 +0200

    Changes to be committed: 	new file:   assets/Cyberpunk-title-glitch.gif 	new file:   assets/Nouveau Document texte.txt 	new file:   assets/cyberpunkbg.jpg 	new file:   assets/cyberpunkgame.jpg 	new file:   assets/title-img.jpg 	new file:   assets/title-mid-img.png 	modified:   index.html 	new file:   script.js 	modified:   style/common-style.css 	modified:   style/index-style.css

---
+++ script.js
@@ -0,0 +1,41 @@
+const titleTop = document.querySelector("#title-top > img");
+const sections = document.querySelectorAll('.section');
+
+let isScrolling = false;
+let currentSection = 0;
+
+window.addEventListener('wheel', function (event) {
+    event.preventDefault();
+    if (isScrolling) return;
+
+    isScrolling = true;
+    console.log('Scroll detected');
+
+    if (event.deltaY > 0 && currentSection < sections.length - 1) {
+        console.log('Scrolling down');
+        currentSection++;
+    } else if (event.deltaY < 0 && currentSection > 0) {
+        console.log('Scrolling up');
+        currentSection--;
+    }
+
+    scrollToSection(currentSection);
+}, { passive: false });
+
+function scrollToSection(sectionIndex) {
+    console.log(sections[sectionIndex].offsetTop);
+    console.log(window);
+    window.scrollTo({
+        top: sections[sectionIndex].offsetTop,
+        behavior: 'smooth'
+    });
+    setTimeout(() => isScrolling = false, 1000);
+}
+
+// Intervalle qui fait que toutes les 2 à 5 secondes, le titre devient l'animation de glitch pendant 0.2 à 0.5 secondes
+setInterval(() => {
+    titleTop.src = "assets/Cyberpunk-title-glitch.gif"
+    setTimeout(() => titleTop.src = "assets/Cyberpunk-title.png", Math.floor(Math.random() * (500 - 200) + 200))
+}
+, Math.floor(Math.random() * (5000 - 2000) + 2000))
+
